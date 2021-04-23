91f33dc8924bc18aa3ded38627cc371c0af1cd31
Subha Chanda <subhachanda88@gmail.com>
2021-04-23 22:38:53 +0530

    Opening Mobile version in new tab

--- background.js
+++ background.js
@@ -1,10 +1,12 @@
 chrome.browserAction.onClicked.addListener(function (tab) {
   chrome.tabs.query({ active: true, lastFocusedWindow: true }, (tabs) => {
     var url = tabs[0];
+    var newUrl = url.url.replace('www', 'm');
     // use `url` here inside the callback because it's asynchronous!
     chrome.tabs.sendMessage(url.id, {
       message: 'browser_action',
       url: url.url,
     });
+    chrome.tabs.create({ url: newUrl });
   });
 });
--- content.js
+++ content.js
@@ -1,5 +1,7 @@
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
   if (request.message === 'browser_action') {
-    console.log(request.url);
+    let url = request.url;
+    url = url.replace('www', 'm');
+    console.log(url);
   }
 });
