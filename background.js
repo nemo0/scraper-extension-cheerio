e13693f945843a51e16faf289ce5a1d35aaeb8bd
Subha Chanda <subhachanda88@gmail.com>
2021-04-24 19:22:20 +0530

    Getting Mobile Source but also desktop one at first

--- background.js
+++ background.js
@@ -1,22 +1,45 @@
 chrome.browserAction.onClicked.addListener(function (tab) {
   chrome.tabs.query({ active: true, lastFocusedWindow: true }, (tabs) => {
     var url = tabs[0];
-    var newUrl = url.url.replace('www', 'm');
     // use `url` here inside the callback because it's asynchronous!
     chrome.tabs.sendMessage(url.id, {
-      message: 'browser_action',
+      message: 'get_me_url',
       url: url.url,
     });
   });
 });
 
-chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
-  if (request.message === 'open_new_tab') {
-    chrome.tabs.update({ url: request.url });
+chrome.runtime.onMessage.addListener(async function (
+  request,
+  sender,
+  sendResponse
+) {
+  if (request.message === 'update_tab') {
+    await chrome.tabs.update({ url: request.url });
+    return true;
   }
-});
-chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
   if (request.message === 'dom') {
-    console.log(request.dom);
+    var dom = request.dom;
+    console.log(dom);
+    return true;
   }
 });
+
+// chrome.runtime.onMessage.addListener((response, sender, sendResponse) => {});
+
+// chrome.runtime.onMessage.addListener(function (tabId, info, tab) {
+//   chrome.tabs.sendMessage(tabId, {
+//     message: 'get_dom',
+//   });
+// });
+
+// async function sendUpdateTabReq() {
+//   await chrome.tabs.sendMessage(sender.tab.id, { message: 'get_me_dom' });
+// }
+
+// chrome.runtime.sendMessage({ message: 'get_me_dom' });
+// chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+//   if (request.message === 'dom') {
+//     console.log(request.dom);
+//   }
+// });
--- content.js
+++ content.js
@@ -1,10 +1,32 @@
-chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
-  if (request.message === 'browser_action') {
+// Adding m in place of www
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+  if (request.message === 'get_me_url') {
     let url = request.url;
     url = url.replace('www', 'm');
-    console.log(url);
-    chrome.runtime.sendMessage({ message: 'open_new_tab', url: url });
-    var html = document.documentElement.outerHTML;
-    chrome.extension.sendMessage({ message: 'dom', dom: html });
+    chrome.runtime.sendMessage({
+      message: 'update_tab',
+      url: url,
+    });
+    return true;
   }
+  // if (request.message === 'get_me_dom') {
+  //   var html = document.documentElement.outerHTML;
+  //   chrome.extension.sendMessage({ message: 'dom', dom: html });
+  // }
 });
+
+// chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+//   if (request.message === 'get_dom') {
+//     var html = document.documentElement.outerHTML;
+//     chrome.extension.sendMessage({ message: 'dom', dom: html });
+//   }
+// });
+
+// chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+//   if (request.message === 'get_me_dom') {
+//     var html = document.documentElement.outerHTML;
+//     chrome.extension.sendMessage({ message: 'dom', dom: html });
+//   }
+// });
+var html = document.documentElement.outerHTML;
+chrome.extension.sendMessage(null, { message: 'dom', dom: html });
